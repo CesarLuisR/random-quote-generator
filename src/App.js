44b1031b5bb3c122a2e8327a7cd37fed186c0816
Cesar Luis Rijo <cesarluisrc@gmail.com>
2021-04-28 17:45:38 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,62 +1,138 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {
   AppContainer,
   AuthorContainer,
   Header,
   HeaderContent,
   HeaderText,
-  InfoContainer,
   QuoteContainer,
   QuoteLine,
   QuoteText,
   GenreContainer,
   Footer,
+  AppAuthorContainer,
 } from "./styles";
 
 const App = () => {
   const [quote, setQuote] = useState({});
+  const [authorQuotes, setAuthorQuotes] = useState();
+  const [author, setAuthor] = useState();
 
-  const getRandomQuote = () => {
-    axios
-      .get("https://quote-garden.herokuapp.com/api/v3/quotes/random")
-      .then((resp) => {
-        const data = resp.data.data[0];
+  const getRandomQuote = useCallback(() => {
+    axios.get("http://api.quotable.io/random").then((resp) => {
+      const data = resp.data;
 
-        setQuote({
-          text: data.quoteText,
-          author: data.quoteAuthor,
-          genre: data.quoteGenre,
-        });
+      setAuthor(data.author);
+
+      setQuote({
+        text: data.content,
+        author: data.author,
+        genre: data.tags[0],
       });
-  };
+    });
+  }, []);
+
+  const getAuthorQuotes = useCallback(async () => {
+    let quotes = [];
+    let url = "http://api.quotable.io/quotes?author=";
+    let name = author.split(" ");
+
+    name.forEach((element) => {
+      url += `%20${element}`;
+    });
+
+    await axios.get(url).then((resp) => {
+      let stack = resp.data.results;
+      stack.forEach((element) => quotes.push(element.content));
+    });
+
+    setAuthorQuotes(quotes);
+  }, [author]);
 
   useEffect(() => {
     getRandomQuote();
-  }, []);
+  }, [getRandomQuote]);
+
+  useEffect(() => {
+    author && getAuthorQuotes();
+  }, [getAuthorQuotes, author]);
 
   return (
-    <AppContainer>
-      <Header>
-        <HeaderContent onClick={getRandomQuote}>
-          <HeaderText>Random</HeaderText>
-          <span className="material-icons">loop</span>
-        </HeaderContent>
-      </Header>
-      <QuoteContainer>
-        <QuoteLine />
-        <QuoteText>{quote.text ? `"${quote.text}"` : "Loading..."}</QuoteText>
-      </QuoteContainer>
-      <InfoContainer>
-        <AuthorContainer>
-          {quote.author ? quote.author : "Loading..."}
-        </AuthorContainer>
-        <GenreContainer>
-          {quote.genre ? quote.genre : "Loading..."}
-        </GenreContainer>
-      </InfoContainer>
-      <Footer>Created by Cesar Luis Rijo - devChallenges.io</Footer>
-    </AppContainer>
+    <Router>
+      <Switch>
+        <Route path="/author">
+          {authorQuotes ? (
+            <AppAuthorContainer>
+              <Header>
+                <HeaderContent onClick={getRandomQuote}>
+                  <HeaderText>Random</HeaderText>
+                  <span className="material-icons">loop</span>
+                </HeaderContent>
+              </Header>
+              {authorQuotes.map((text, index) =>
+                index === 0 ? (
+                  <QuoteContainer>
+                    <h1 className={"author-name"}>{author}</h1>
+                    <div className="none"></div>
+                    <QuoteLine />
+                    <QuoteText>{text}</QuoteText>
+                  </QuoteContainer>
+                ) : (
+                  <QuoteContainer>
+                    <QuoteLine />
+                    <QuoteText>{text}</QuoteText>
+                  </QuoteContainer>
+                )
+              )}
+              <Footer>Created by Cesar Luis Rijo - devChallenges.io</Footer>
+            </AppAuthorContainer>
+          ) : (
+            <AppAuthorContainer>
+              <Header>
+                <HeaderContent onClick={getRandomQuote}>
+                  <HeaderText>Loading...</HeaderText>
+                </HeaderContent>
+              </Header>
+              <Footer>Created by Cesar Luis Rijo - devChallenges.io</Footer>
+            </AppAuthorContainer>
+          )}
+        </Route>
+        <Route path="/">
+          <AppContainer>
+            <Header>
+              <HeaderContent onClick={getRandomQuote}>
+                <HeaderText>Random</HeaderText>
+                <span className="material-icons">loop</span>
+              </HeaderContent>
+            </Header>
+            <QuoteContainer>
+              <QuoteLine />
+              <QuoteText>
+                {quote.text ? `"${quote.text}"` : "Loading..."}
+              </QuoteText>
+              <Link
+                to="/author"
+                className={"infoContainer"}
+                onClick={getAuthorQuotes}
+              >
+                <div className={"info"}>
+                  <AuthorContainer className={"authorContainer"}>
+                    {quote.author ? quote.author : "Loading..."}
+                  </AuthorContainer>
+                  <GenreContainer>
+                    {quote.genre ? quote.genre : "Loading..."}
+                  </GenreContainer>
+                </div>
+                <span className={"material-icons arrow"}>arrow_right_alt</span>
+              </Link>
+            </QuoteContainer>
+            <Footer>Created by Cesar Luis Rijo - devChallenges.io</Footer>
+          </AppContainer>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/styles.js
+++ src/styles.js
@@ -5,8 +5,15 @@ const lineColor = "#F7DF94";
 
 export const AppContainer = styled.div`
   width: 85%;
-  margin: 32px auto;
+  margin: 0 auto;
+  padding-top: 32px;
   max-width: 85%;
+  position: relative;
+  min-height: 100vh;
+`;
+
+export const AppAuthorContainer = styled(AppContainer)`
+  padding-bottom: 40px;
 `;
 
 export const Header = styled.header`
@@ -35,10 +42,62 @@ export const HeaderText = styled.div`
 
 export const QuoteContainer = styled.div`
   width: 50%;
-  height: max-content;
   margin: 0 auto;
   display: grid;
   grid-template-columns: 8px 1fr;
+  grid-template-rows: max-content 1fr;
+  margin-bottom: 100px;
+
+  .author-name {
+    width: max-content;
+    margin-bottom: 100px;
+    font-family: Raleway;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 42px;
+    color: #333333;
+  }
+
+  .infoContainer {
+    grid-column: 1 / 3;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 80px;
+    margin-left: 56px;
+    width: 100%;
+    padding: 29px 50px;
+    cursor: pointer;
+    transition: 0.3s;
+    margin-bottom: 100px;
+    text-decoration: none;
+
+    &:hover {
+      background: #333333;
+
+      .authorContainer {
+        transition: 0.3s;
+        color: #f2f2f2;
+      }
+
+      .arrow {
+        display: block;
+      }
+    }
+
+    .arrow {
+      display: none;
+      color: white;
+    }
+
+    @media (max-width: 600px) {
+      margin-left: 20px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    width: 90%;
+  }
 `;
 
 export const QuoteLine = styled.div`
@@ -56,13 +115,10 @@ export const QuoteText = styled.div`
   font-size: 32px;
   line-height: 120%;
   color: #000000;
-`;
 
-export const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 80px;
-  margin-left: 446px;
+  @media (max-width: 600px) {
+    margin-left: 50px;
+  }
 `;
 
 export const AuthorContainer = styled.div`
@@ -91,5 +147,6 @@ export const Footer = styled.footer`
   line-height: 17px;
   text-align: center;
   color: #828282;
-  margin-top: 80px;
+  position: absolute;
+  bottom: 15px;
 `;
